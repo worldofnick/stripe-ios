5b6a3d7563b2fd08e757b9fcca07ce6e656f1114
Nick Porter <worldofnick@users.noreply.github.com>
2026-08-08 09:21:25 -0600

    Trim duplicate CustomerSheet RTL snapshot variants

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/CustomerSheetSnapshotTests.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/CustomerSheetSnapshotTests.swift
@@ -54,10 +54,8 @@ class StubCustomerAdapter: CustomerAdapter {
 class CustomerSheetSnapshotTests: STPSnapshotTestCase {
 
     private var cs: CustomerSheet!
-    private var testWindow: UIWindow?
-
-    private func makeWindow(size: CGSize = CGSize(width: 428, height: 1026)) -> UIWindow {
-        let window = UIWindow(frame: CGRect(origin: .zero, size: size))
+    private var window: UIWindow {
+        let window = UIWindow(frame: CGRect(x: 0, y: 0, width: 428, height: 1026))
         window.isHidden = false
         return window
     }
@@ -65,7 +63,6 @@ class CustomerSheetSnapshotTests: STPSnapshotTestCase {
     public override func tearDown() {
         super.tearDown()
         HTTPStubs.removeAllStubs()
-        testWindow = nil
     }
 
     private func stubbedAPIClient() -> STPAPIClient {
@@ -83,41 +80,6 @@ class CustomerSheetSnapshotTests: STPSnapshotTestCase {
         stubSessions(paymentMethods: "\"card\"")
         prepareCS(configuration: configuration())
         presentCS(darkMode: false)
-        let view = cs.bottomSheetViewController.view!
-        view.semanticContentAttribute = .forceRightToLeft
-        view.layoutIfNeeded()
-        verify(view)
-    }
-
-    func testRightToLeftLandscape() {
-        stubSessions(paymentMethods: "\"card\"")
-        prepareCS(configuration: configuration())
-        presentCS(
-            darkMode: false,
-            windowSize: CGSize(width: 844, height: 390),
-            additionalTraits: [
-                UITraitCollection(horizontalSizeClass: .compact),
-                UITraitCollection(verticalSizeClass: .compact),
-            ]
-        )
-        let view = cs.bottomSheetViewController.view!
-        view.semanticContentAttribute = .forceRightToLeft
-        view.layoutIfNeeded()
-        verify(view)
-    }
-
-    func testRightToLeftIPad() {
-        stubSessions(paymentMethods: "\"card\"")
-        prepareCS(configuration: configuration())
-        presentCS(
-            darkMode: false,
-            windowSize: CGSize(width: 1024, height: 1366),
-            additionalTraits: [
-                UITraitCollection(userInterfaceIdiom: .pad),
-                UITraitCollection(horizontalSizeClass: .regular),
-                UITraitCollection(verticalSizeClass: .regular),
-            ]
-        )
         let view = cs.bottomSheetViewController.view!
         view.semanticContentAttribute = .forceRightToLeft
         view.layoutIfNeeded()
@@ -545,16 +507,6 @@ class CustomerSheetSnapshotTests: STPSnapshotTestCase {
         verify(view)
     }
 
-    func testRightToLeftDynamicType() {
-        stubSessions(paymentMethods: "\"card\"")
-        prepareCS(configuration: configuration())
-        presentCS(darkMode: false, preferredContentSizeCategory: .accessibilityExtraExtraLarge)
-        let view = cs.bottomSheetViewController.view!
-        view.semanticContentAttribute = .forceRightToLeft
-        view.layoutIfNeeded()
-        verify(view)
-    }
-
     func testOneSavedCardPMDarkMode() {
         stubSessions(paymentMethods: "\"card\"")
         let customerAdapter = StubCustomerAdapter()
@@ -663,16 +615,10 @@ class CustomerSheetSnapshotTests: STPSnapshotTestCase {
         self.cs = CustomerSheet(configuration: configuration, customer: customerAdapter)
     }
 
-    private func presentCS(
-        darkMode: Bool,
-        preferredContentSizeCategory: UIContentSizeCategory = .large,
-        windowSize: CGSize = CGSize(width: 428, height: 1026),
-        additionalTraits: [UITraitCollection] = []
-    ) {
+    private func presentCS(darkMode: Bool, preferredContentSizeCategory: UIContentSizeCategory = .large) {
         let vc = UIViewController()
         let navController = UINavigationController(rootViewController: vc)
-        let testWindow = makeWindow(size: windowSize)
-        self.testWindow = testWindow
+        let testWindow = self.window
         if darkMode {
             testWindow.overrideUserInterfaceStyle = .dark
         }
@@ -701,9 +647,7 @@ class CustomerSheetSnapshotTests: STPSnapshotTestCase {
         wait(for: [loadFinishedExpectation], timeout: 5)
 
         cs.bottomSheetViewController.presentationController!.overrideTraitCollection = UITraitCollection(
-            traitsFrom: [
-                UITraitCollection(preferredContentSizeCategory: preferredContentSizeCategory),
-            ] + additionalTraits
+            preferredContentSizeCategory: preferredContentSizeCategory
         )
     }
 
